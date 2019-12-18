7ca4ab46e00cf2767916b0b6b9d16ba9043d5b92
EC2 Default User <ec2-user@ip-172-31-38-174.us-east-2.compute.internal>
2019-12-18 01:31:31 +0000

    delete_function

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,8 +7,22 @@
     
     @include('errors.form_errors')
     
-    {!! Form::model($article, ['method' => 'PATCH', 'url' => ['articles', $article->id]]) !!}
-        @include('creates.form', ['published_at' => $article->published_at->format('Y-m-d'), 'submitButton' => 'Create Article'])
-    {!! Form::close() !!}
+    {!! Form::open(['url'=>'articles']) !!}
+        <div class="form-group">
+            {!! Form::label('title','Title:')!!}
+            {!! Form::text('title', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('body','Body:')!!}
+            {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('published_at','Publish On:')!!}
+            {!! Form::input('date', 'published_at', date('Y-m-d'), ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Add Article', ['class'=>'btn btn-primary form-control']) !!}
+        </div>
+        {!! Form::close() !!}
     @endsection
         
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -16,9 +16,7 @@
             編集
         </a>
         
-        {{!! Form::open(['method' => 'DELETE', 'url' => ['articles', $article->id], 'class' => 'd-inline']) !!}
-            {!! Form::submit('削除', ['class'=> 'btn btn-danger']) !!}
-        {!! Form::close() !!}
+        {!! delete_form(['articles', $article->id] !!)}
         <a href= "{{ action('ArticlesController@index')}}" class="btn btn-secondary float-right">
         一覧へ戻る
         </a>
